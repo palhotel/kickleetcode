3947ed966dda64f964f92b2f42f2b261ebbb3d96
palhotel <258838102@qq.com>
2017-06-28 18:10:08 +0800

    solve database 176 177

---
+++ kickleetcode/Problems/Database/N176.sql
@@ -0,0 +1,2 @@
+﻿select max(Salary) as SecondHighestSalary from Employee 
+ where Salary not in (select max(Salary) as SecondHighestSalary from Employee) 
---
+++ kickleetcode/Problems/Database/N177.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+      DECLARE M INT Default 0; 
+      set M = N - 1;
+  RETURN (
+
+      # Write your MySQL query statement below.
+       select DISTINCT Salary as SecondHighestSalary from Employee ORDER BY Salary DESC limit 1 OFFSET M
+  );
+END
