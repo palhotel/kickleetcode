30eaccd36b7bebb78bb595b0cc4d29afb01c113e
palhotel <258838102@qq.com>
2017-07-04 11:07:20 +0800

    solve database 181

---
+++ kickleetcode/Problems/Database/N181.sql
@@ -0,0 +1,4 @@
+﻿# Write your MySQL query statement below
+select t1.Name as Employee
+from employee t1, employee t2
+where t1.ManagerId is not null and t1.ManagerId = t2.Id and t1.Salary > t2.Salary
