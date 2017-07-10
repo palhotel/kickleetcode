21d2e81921fb5abe2c0dd1a367b6f89fe5590e4c
palhotel <258838102@qq.com>
2017-07-10 09:22:08 +0800

    solve database 184

---
+++ kickleetcode/Problems/Database/N184.sql
@@ -0,0 +1,6 @@
+﻿select Department.Name as Department, Employee.`Name` as Employee, employee.Salary
+from department
+join employee ON 
+employee.DepartmentId = department.Id 
+and 
+employee.Salary in (select max(t.salary) from employee t where t.DepartmentId = department.Id )
