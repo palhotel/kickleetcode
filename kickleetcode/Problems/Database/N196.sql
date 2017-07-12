eeefa21d47bc564e42252ab2d31d4652ac8f2f38
palhotel <258838102@qq.com>
2017-07-12 09:00:11 +0800

    solve database 196 and 197

---
+++ kickleetcode/Problems/Database/N196.sql
@@ -0,0 +1,14 @@
+﻿delete From person
+where person.id in (
+select fuck.id from(
+select p1.id  from person p1
+where p1.email in 
+(select email from person p2
+ where p2.email = p1.email and p1.id > p2.id)
+) fuck
+)
+-- better solution
+-- DELETE p1
+-- FROM Person p1, Person p2
+-- WHERE p1.Email = p2.Email AND
+-- p1.Id > p2.Id
---
+++ kickleetcode/Problems/Database/N197.sql
@@ -0,0 +1,3 @@
+﻿SELECT w1.Id from weather w1, weather w2
+where w2.Date = DATE_ADD(w1.Date,INTERVAL -1 DAY)
+and w1.Temperature > w2.Temperature
