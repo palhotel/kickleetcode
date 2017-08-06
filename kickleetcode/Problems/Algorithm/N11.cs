f958742e89f050c7627dd86ecff730a3e7276bcb
palhotel <258838102@qq.com>
2017-08-06 22:22:29 +0800

    solve algorithm 11

---
+++ kickleetcode/Problems/Algorithm/N11.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Container With Most Water
+    /// </summary>
+    class N11
+    {
+        public int MaxArea(int[] height) {
+            int left = 0, right = height.Length - 1;
+            int maxArea = 0;
+
+            while (left < right) {
+                maxArea = Math.Max(maxArea, Math.Min(height[left], height[right])
+                        * (right - left));
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+                    
+            }
+
+            return maxArea;
+        }
+    }
+}
