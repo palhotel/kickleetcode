b4fc1ac6859efbd17cce5b9dee94888976f607a3
palhotel <258838102@qq.com>
2017-08-11 14:56:18 +0800

    solve algorithm 17 and 18

---
+++ kickleetcode/Problems/Algorithm/N17.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Letter Combinations of a Phone Number
+    /// </summary>
+    class N17
+    {
+        public IList<string> LetterCombinations(string digits)
+        {
+            string[] keyboard =
+            {
+                " ",
+                "!",
+                "abc",
+                "def",
+                "ghi",
+                "jkl",
+                "mno",
+                "pqrs",
+                "tuv",
+                "wxyz"
+            };
+            List<string> list = new List<string>();
+
+            string outstr = "";
+            Output(digits, outstr, list, keyboard, 0);
+            return list;
+        }
+
+        public void Output(string src, string dest, List<string> list, string[] keyboard, int cur)
+        {
+            if (src.Length - 1 < cur)
+            {
+                if (!string.IsNullOrEmpty(dest))
+                {
+                    list.Add(dest);
+                }
+                dest = "";
+                return;
+            }
+            int nCur = src[cur] - '0';
+            string newdest = dest;
+            for (int i = 0; i < keyboard[nCur].Length; i++)
+            {
+                dest = newdest + keyboard[nCur][i];
+                Output(src, dest, list, keyboard, cur + 1);
+            }
+        }
+    }
+}
---
+++ kickleetcode/Problems/Algorithm/N18.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// 4Sum
+    /// </summary>
+    class N18
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            List<IList<int>> list = new List<IList<int>>();
+            if (nums.Length >= 4)
+            {
+                Array.Sort(nums);
+                for (int i = 0; i <= nums.Length - 4; i++) {
+                    if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+                    {
+                        int sum = target - nums[i];
+                        var listtemp = ThreeSum(nums, i+1, sum);
+                        for (int k = 0; k < listtemp.Count; k++)
+                        {
+                            list.Add(new int[] { nums[i], listtemp[k][0], listtemp[k][1], listtemp[k][2] });
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        public IList<IList<int>> ThreeSum(int[] nums, int start, int target) {
+            List<IList<int>> list = new List<IList<int>>();
+            if (nums.Length >= 3) {
+                Array.Sort(nums);
+                for (int i = start; i < nums.Length - 2; i++) {
+                    if (i == start || (i > start && nums[i] != nums[i - 1])) {
+                        //第一个数字和前边不一样
+                        //0 - 第一个数
+                        int lo = i + 1, hi = nums.Length - 1, sum = target - nums[i];
+                        while (lo < hi) {
+                            if (nums[lo] + nums[hi] == sum) {
+                                list.Add(new int[] { nums[i], nums[lo], nums[hi] });
+                                while (lo < hi && nums[lo] == nums[lo + 1]) {
+                                    //跳过所有相同的
+                                    lo++;
+                                }
+                                while (lo < hi && nums[hi] == nums[hi - 1]) {
+                                    //跳过所有相同的
+                                    hi--;
+                                }
+                                lo++; hi--;
+                            } else if (nums[lo] + nums[hi] < sum) {
+                                lo++;
+                            } else {
+                                hi--;
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
