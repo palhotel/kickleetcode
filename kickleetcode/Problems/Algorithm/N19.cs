52094085a75338d13d0aa52ff1057ed36f5cdc53
palhotel <258838102@qq.com>
2017-08-18 09:46:41 +0800

    solve algorithm 19 and 20

---
+++ kickleetcode/Problems/Algorithm/N19.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Remove Nth Node From End of List
+    /// </summary>
+    class N19
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n) {
+            if (n == 0) {
+                return head;
+            }
+            ListNode p1 = head;
+            ListNode p2 = p1;
+            while (n > 0) {
+                p2 = p2.next;
+                n--;
+                if (p2 == null) {
+                    break;
+                }
+            }
+            if (p2 == null && n == 0) {
+                return p1.next;
+            }
+            while (p2.next != null) {
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+
+            if (p1.next == null) {
+                return null;
+            } else {
+                p1.next = p1.next.next;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ kickleetcode/Problems/Algorithm/N20.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Valid Parentheses
+    /// </summary>
+    class N20
+    {
+        public bool IsValid(string s) {
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i] != ')' && s[i] != ']' && s[i] != '}') {
+                    stack.Push(s[i]);
+                } else {
+                    if (stack.Count == 0) {
+                        return false;
+                    }
+                    char left = stack.Pop();
+
+                    if (s[i] == ')' && left == '(') {
+                        continue;
+                    } else if (s[i] == ']' && left == '[') {
+                        continue;
+                    } else if (s[i] == '}' && left == '{') {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
