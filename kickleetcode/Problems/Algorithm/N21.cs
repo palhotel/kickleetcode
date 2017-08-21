2e45bad5157b62a48533c62c1fb3e7d67bdd985a
palhotel <258838102@qq.com>
2017-08-21 16:38:24 +0800

    solve algorithm 21 22

---
+++ kickleetcode/Problems/Algorithm/N21.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Merge Two Sorted Lists
+    /// </summary>
+    class N21
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode p = new ListNode(-1);
+            ListNode q = p;
+            while (l1 != null || l2 != null) {
+                if (l1 == null) {
+                    q.next = l2;
+                    break;
+                } else if (l2 == null) {
+                    q.next = l1;
+                    break;
+                } else {
+                    if (l1.val < l2.val) {
+                        q.next = l1;
+                        q = q.next;
+                        l1 = l1.next;
+                    } else {
+                        q.next = l2;
+                        q = q.next;
+                        l2 = l2.next;
+                    }
+                }
+            }
+            return p.next;
+        }
+    }
+}
---
+++ kickleetcode/Problems/Algorithm/N22.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Generate Parentheses
+    /// </summary>
+    class N22
+    {
+        public IList<string> GenerateParenthesis(int n) {
+            List<String> list = new List<String>();
+            //
+            generateOneByOne("", list, n, n);
+            return list;
+        }
+        public void generateOneByOne(String sublist, List<String> list, int left, int right) {
+            if (left > right) {
+                return;
+            }
+            if (left > 0) {
+                generateOneByOne(sublist + "(", list, left - 1, right);
+            }
+            if (right > 0) {
+                generateOneByOne(sublist + ")", list, left, right - 1);
+            }
+            if (left == 0 && right == 0) {
+                //匹配完毕
+                list.Add(sublist);
+                return;
+            }
+        }
+    }
+}
