85e43e755620b3ad2a7afd6401988ebb296776ce
palhotel <258838102@qq.com>
2017-08-29 17:59:32 +0800

    sovle algorithm 24 25

---
+++ kickleetcode/Problems/Algorithm/N24.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Swap Nodes in Pairs
+    /// </summary>
+    class N24
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            ListNode p1 = head, p2 = head;
+            ListNode realHead = new ListNode(-1);
+            realHead.next = head;
+            ListNode ptemp = realHead;
+
+            while (p1 != null && p1.next != null)
+            {
+                p2 = p1.next;
+
+                p1.next = p2.next;
+                p2.next = p1;
+                ptemp.next = p2;
+
+                ptemp = p1;
+                p1 = p1.next;
+            }
+
+            return realHead.next;
+        }
+    }
+}
---
+++ kickleetcode/Problems/Algorithm/N25.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Reverse Nodes in k-Group
+    /// </summary>
+    class N25
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            ListNode p1 = head, p2 = head;
+            ListNode realHead = new ListNode(-1);
+            realHead.next = head;
+            ListNode pkhead= realHead;
+            ListNode temp = null;
+
+
+            while (isNeed(p1, k))
+            {
+                int n = k - 1;
+                p2 = p1.next;
+
+                while (n > 0)
+                {
+                    pkhead.next = p2;
+                    temp = p2.next;
+                    p2.next = p1;
+
+                    p1 = p2;
+                    p2 = temp;
+                    n --;
+                }
+
+                p1 = p2;
+                n = k;
+                while (n-- > 0)
+                {
+                    pkhead = pkhead.next;
+                }
+                pkhead.next = p1;
+            }
+            return realHead.next;
+        }
+
+        public bool isNeed(ListNode head, int k)
+        {
+            while (k > 0)
+            {
+                if (head == null)
+                {
+                    return false;
+                }
+                head = head.next;
+                k--;
+            }
+            return true;
+        }
+    }
+}
