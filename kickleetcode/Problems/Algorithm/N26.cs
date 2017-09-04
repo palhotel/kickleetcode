25cfcb08029d7270c69eb2a4c68987172a0ef99e
palhotel <258838102@qq.com>
2017-09-04 18:21:26 +0800

    solve algorithm 26

---
+++ kickleetcode/Problems/Algorithm/N26.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Remove Duplicates from Sorted Array
+    /// </summary>
+    class N26
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int length = nums.Length;
+            int last = 1;
+            int count = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    nums[last++] = nums[i];
+                }
+                else
+                {
+                    count ++;
+                }
+            }
+            return length - count;
+        }
+    }
+}
