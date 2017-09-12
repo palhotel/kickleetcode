3e61a03ba5c60148710b92d88e4db05faa1cb4c7
palhotel <258838102@qq.com>
2017-09-12 11:27:03 +0800

    sovle algorithm 32

---
+++ kickleetcode/Problems/Algorithm/N32.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Longest Valid Parentheses
+    /// </summary>
+    class N32
+    {
+        public int LongestValidParentheses(string s) {
+            int maxans = 0;
+            Stack<int> stack = new Stack<int>();
+            stack.Push(-1);
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i] == '(') {
+                    stack.Push(i);
+                } else {
+                    stack.Pop();
+                    if (stack.Count == 0) {
+                        stack.Push(i);
+                    } else {
+                        maxans = Math.Max(maxans, i - stack.Peek());
+                    }
+                }
+            }
+            return maxans;
+        }
+    }
+}
