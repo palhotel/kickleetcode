8209df2b34d27f08931c6c471dadc1ef1082a12b
palhotel <258838102@qq.com>
2017-09-19 16:33:08 +0800

    solve algorithm 36 37

---
+++ kickleetcode/Problems/Algorithm/N36.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Valid Sudoku
+    /// </summary>
+    class N36
+    {
+        public bool IsValidSudoku(char[,] board)
+        {
+            int n = 9;
+            int m = 0;
+            HashSet<char> set = new HashSet<char>();
+            for (int i = 0; i < n; i++)
+            {
+                set.Clear();
+                for (int j = 0; j < n; j++)
+                {
+                    if (board[i, j] != '.')
+                    {
+                        bool isUnique = set.Add(board[i, j]);
+                        if (!isUnique) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            for (int j = 0; j < n; j++)
+            {
+                set.Clear();
+                for (int i = 0; i < n; i++)
+                {
+                    if (board[i, j] != '.')
+                    {
+                        bool isUnique = set.Add(board[i, j]);
+                        if (!isUnique) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < 9; i= i + 3)
+            {
+                for (int j = 0; j < 9; j = j + 3)
+                {
+                    set.Clear();
+                    
+                    for (int k = i; k < i + 3; k++)
+                    {
+                        for (int l = j; l < j + 3; l ++)
+                        {
+                            if (board[k, l] != '.')
+                            {
+                                bool isUnique = set.Add(board[k, l]);
+                                if (!isUnique) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ kickleetcode/Problems/Algorithm/N37.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Sudoku Solver
+    /// </summary>
+    class N37
+    {
+        public void SolveSudoku(char[,] board) {
+            if (board == null || board.Length == 0)
+                return;
+            Solve(board);
+        }
+
+        public bool Solve(char[,] board) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (board[i,j] == '.') {
+                        for (char c = '1'; c <= '9'; c++) {
+                            if (IsValid(board, i, j, c)) {
+                                board[i,j] = c;
+
+                                if (Solve(board))
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    board[i, j] = '.';
+                                }   
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool IsValid(char[,] board, int row, int col, char c) {
+
+            for (int i = 0; i < 9; i++) {
+                if (board[i,col] == c) {
+                    return false; //check row
+                }
+                else if (board[row,i] == c) {
+                    return false; //check column
+                }
+                else if (board[3*(row/3) + i/3, 3*(col/3) + i%3] == c)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
