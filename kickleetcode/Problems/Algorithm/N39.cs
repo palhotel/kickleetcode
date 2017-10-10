31aa8484cdb8eac3297beb11ab5f64b73159dcf8
palhotel <258838102@qq.com>
2017-10-10 10:33:20 +0800

    sovle algorithm 39

---
+++ kickleetcode/Problems/Algorithm/N39.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Combination Sum
+    /// </summary>
+    class N39
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            Array.Sort(candidates);
+            List<IList<int>> result = new List<IList<int>>();
+            getResult(result, candidates, new List<int>(), target, 0);
+            return result;
+        }
+
+        public void getResult(IList<IList<int>> list, int[] candidates, IList<int> cur, int num, int idx)
+        {
+            if (num > 0)
+            {
+                for (int i = idx; i < candidates.Length; i++)
+                {
+                    if (candidates[i] <= num)
+                    {
+                        cur.Add(candidates[i]);
+                        getResult(list, candidates, cur, num - candidates[i], i);
+                    }
+                    else
+                    {
+                        if (cur.Count > 0)
+                        {
+                            cur.RemoveAt(cur.Count - 1);
+                        }
+                        return;
+                    }
+                }
+
+                if (cur.Count > 0) {
+                    cur.RemoveAt(cur.Count - 1);
+                }
+            } else if (num == 0)
+            {
+                List<int> clone = new List<int>(cur);
+                list.Add(clone);
+                if (cur.Count > 0)
+                {
+                    cur.RemoveAt(cur.Count - 1);
+                }
+            }
+        }
+    }
+}
