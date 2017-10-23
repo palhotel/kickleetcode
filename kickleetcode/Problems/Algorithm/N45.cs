946c26e38bbe7e2a67415dc38ee35566fa1d6f17
palhotel <258838102@qq.com>
2017-10-23 18:07:43 +0800

    solve algorithm 45

---
+++ kickleetcode/Problems/Algorithm/N45.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    /// <summary>
+    /// Jump Game II
+    /// </summary>
+    class N45
+    {
+        public int Jump(int[] nums) {
+            int jumps = 0, curEnd = 0, curFarthest = 0;
+            for (int i = 0; i < nums.Length - 1; i++) {
+                curFarthest = Math.Max(curFarthest, i + nums[i]);
+                if (i == curEnd) {
+                    jumps++;
+                    curEnd = curFarthest;
+                }
+            }
+            return jumps;
+        }
+    }
+}
