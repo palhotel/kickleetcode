9d1bcc64c5082dd0a3b435689f6524af6bff124f
palhotel <258838102@qq.com>
2018-01-05 11:11:54 +0800

    solve algorithm 47

---
+++ kickleetcode/Problems/Algorithm/N47.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kickleetcode.Problems.Algorithm
+{
+    class N47
+    {
+
+        public IList<IList<int>> PermuteUnique(int[] nums) {
+            var lists = new List<IList<int>>();
+            if (nums == null || nums.Length == 0) {
+                return lists;
+            }
+            permute(lists, nums, 0);
+            return lists;
+        }
+
+        private void permute(IList<IList<int>> lists, int[] nums, int index) {
+            if (index == nums.Length) {
+                var list = new List<int>();
+                foreach (var n in nums) {
+                    list.Add(n);
+                }
+                lists.Add(list);
+                return;
+            }
+
+            HashSet<int> appeared = new HashSet<int>();
+
+            for (int i = index; i < nums.Length; ++i) {
+                if (appeared.Add(nums[i])) {
+                    swap(nums, index, i);
+                    permute(lists, nums, index + 1);
+                    swap(nums, index, i);
+                }
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+    }
+}
